3e843653245595787efeee3c7c02a5af06a8c404
maxsl <maxsl.meng@gmail.com>
2024-03-11 20:02:28 +0800

    修改风格

--- backend-server/application/src/main/java/com/apitable/interfaces/billing/BillingContextConfig.java
+++ backend-server/application/src/main/java/com/apitable/interfaces/billing/BillingContextConfig.java
@@ -20,7 +20,6 @@ package com.apitable.interfaces.billing;
 
 import com.apitable.interfaces.billing.facade.EntitlementServiceFacade;
 import com.apitable.interfaces.billing.facade.MaxslEntitlementServiceFacadeImpl;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +29,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration(proxyBeanMethods = false)
 public class BillingContextConfig {
+    
     @Bean
     @ConditionalOnMissingBean
     public EntitlementServiceFacade maxslEntitlementServiceFacade() {
